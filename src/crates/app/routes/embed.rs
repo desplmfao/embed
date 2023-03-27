60e3456d4986d12b37ea1845e5173a5350906ba4
desplmfao <98325692+desplmfao@users.noreply.github.com>
2023-03-26 22:40:10 -0400

    Add files via upload

--- src/crates/app/routes/embed.rs
+++ src/crates/app/routes/embed.rs
@@ -1,6 +1,7 @@
 // ########### original idea from shirtjs ###########
 
 pub async fn embed(
+    client: actix_web::web::Data<awc::Client>,
     request: actix_web::HttpRequest,
     hb: actix_web::web::Data<handlebars::Handlebars<'_>>
 ) -> actix_web::HttpResponse {
@@ -11,20 +12,52 @@ pub async fn embed(
         return actix_web::HttpResponse::NoContent().finish();
     }
 
-    let q_url = urlencoding::decode(q_urlr).unwrap();
+    let q_url = urlencoding::decode(&q_urlr.to_string()).unwrap().to_string();
 
     if request.uri().path_and_query().unwrap().to_string().contains(&"/-?url=") {
         let data = serde_json::json!({
-            "url": ("/-.mp4?url=".to_owned() + &q_url.to_string()),
-            "media_url": urlencoding::decode(&q_url.to_string()).unwrap()
+            "url": ("/-.mp4?url=".to_owned() + &q_url),
+            "media_url": urlencoding::decode(&q_url).unwrap()
         });
 
         let body = hb.render("embed", &data).unwrap();
     
         actix_web::HttpResponse::Ok().body(body)
     } else {
-        return actix_web::HttpResponse::PermanentRedirect()
-            .append_header(("location", urlencoding::decode(&q_url.to_string()).unwrap().to_string()))
-            .finish()
+        if q_url.starts_with("https://cdn.discordapp.com/attachments/") {
+            let mut response = match client
+            .request(
+                actix_web::HttpRequest::method(&request).clone(),
+                q_urlr.to_string().to_owned(),
+            )
+            .send()
+            .await
+        {
+            Ok(resp) => resp,
+            Err(e) => {
+                println!("{:?}", e);
+                return actix_web::HttpResponse::InternalServerError().finish();
+            }
+        };
+
+        return actix_web::HttpResponse::Ok()
+            .body(
+                match response
+                    .body()
+                    .limit(1024 * 1024 * 1024 * 1024)
+                    .await
+                {
+                    Ok(resp) => resp.to_vec(),
+                    Err(e) => {
+                        println!("{:?}", e);
+                        return actix_web::HttpResponse::InternalServerError().finish();
+                    }
+                },
+            );
+        } else {
+            return actix_web::HttpResponse::PermanentRedirect()
+                .append_header(("location", urlencoding::decode(&q_url.to_string()).unwrap().to_string()))
+                .finish()
+        }
     }
 }
